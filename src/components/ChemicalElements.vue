934f8abfa62ae949bcb8ca46bd0cb898c5f13f57
Henry Villa <evilla@Henrys-MBP.fritz.box>
2020-02-05 22:00:45 +0100

    create project
    create proejct with vue, add bootstrap, add google fonts, create chemical-elements, add data.json, add colors to groups, add cssgrid for layout, add basic styles

---
+++ src/components/ChemicalElements.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="chemical-elements">
+    <div
+      :class="[
+        'chemical-element__wrapper',
+        'prd' + chemical.row + 'gp' + chemical.column
+      ]"
+      v-for="(chemical, index) in periodicList"
+      :key="index"
+    >
+      <i class="chemical-element__atomic-number">{{ chemical.atomicNumber }}</i>
+      <span class="chemical-element__symbol">{{ chemical.symbol }}</span>
+      <span class="chemical-element__name">{{ chemical.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chemical-elements",
+  props: ["periodicList"]
+};
+</script>
